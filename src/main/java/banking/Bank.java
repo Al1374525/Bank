93a4cd50c95a2f8a7ac466faf52d1b340e9b62e5
Al1374525 <albertayiteyadjin@gmail.com>
2022-04-19 22:06:31 -0400

    checking if the print statment

--- src/main/java/banking/Bank.java
+++ src/main/java/banking/Bank.java
@@ -1,6 +1,14 @@
 package main.java.banking;
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 
 public class Bank{
-    
+    public static void main(String[] args){
+      //we are checking if this scanner object is functional.
+        Scanner output = new Scanner("This class is functional");
+        System.out.println(output);
+
+    }
 }
--- src/test/java/banking/testBank.java
+++ src/test/java/banking/testBank.java
@@ -1,5 +1,10 @@
 package test.java.banking;
+import java.util.Scanner;
 
 public class testBank {
+
+    public static void main(String[] args){
+
+    }
     
 }
